09e6b2667908c0384a431c0a20cdbe4891a10ea6
CarryRee <63755364@qq.com>
2023-11-20 15:01:18 +0800

    #数据库注释

--- resource/photo.sql
+++ resource/photo.sql
@@ -10,11 +10,11 @@ IF
 	
 	/*如果你的应用有德语、法语或者俄语，请一定使用utf8_unicode_ci。一般用utf8_general_ci就够了*/
 CREATE TABLE `photo` (
-	`id` int(12) NOT NULL auto_increment,
-	`user_id` VARCHAR ( 32 ) NOT NULL,
-	`photo_path` VARCHAR ( 1024 ),
-	`remark` TEXT,
-	`create_time` TIMESTAMP DEFAULT CURRENT_TIMESTAMP ,
-	`update_time` TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+	`id` int(12) NOT NULL auto_increment COMMENT '自增主键', 
+	`user_id` VARCHAR ( 32 ) NOT NULL COMMENT '用户id',
+	`photo_path` VARCHAR ( 1024 ) COMMENT '图片路径',
+	`remark` TEXT COMMENT '备注',
+	`create_time` TIMESTAMP DEFAULT CURRENT_TIMESTAMP COMMENT '创建时间',
+	`update_time` TIMESTAMP DEFAULT CURRENT_TIMESTAMP COMMENT '更新时间',
 	PRIMARY KEY ( `id` )
-) ENGINE = INNODB CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Dynamic;
+) ENGINE = INNODB CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Dynamic COMMENT '图片上传信息表';
